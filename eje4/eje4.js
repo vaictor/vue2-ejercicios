906621e4ce69c26ad4b913706e763baf2b6d623d
Victor Medina <vmedina@ucol.mx>
2022-02-23 16:18:35 -0600

    Iniamos con ejercicios vue 2

--- eje4/eje4.js
+++ eje4/eje4.js
@@ -1,26 +1,28 @@
 var app = new Vue({
     el: '#parteReactivaVue',
     data: {
-        producto: 'MAC book air',
-        materiales: [
+        producto: 'Curso vue ',
+        lstMateriales: [
             {
                 id_material: 250,
                 nombre_material: 'Lectura 1'
             },
             {
                 id_material: 251,
-                nombre_material: 'Grabación meet de clase'
+                nombre_material: 'Grabación de clase en meet'
             },
             {
                 id_material: 252,
                 nombre_material: 'Grabación meet de clase 2'
             }
         ],
-        actividades: [
+        lstActividades: [
                 'Actiivdad inicial',
                 'Actiivdad secundaria',
                 'Actiivdad cuerpo',
-                'Actiivdad final'
+                'Actiivdad final',
+                'Ordinario',
+                'Extaordinario'
         ]
     }
 })
