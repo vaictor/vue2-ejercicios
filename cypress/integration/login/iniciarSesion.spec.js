111708dddf28151afa9cd238d5a5178fb3a2b538
Victor Medina <vmedina@ucol.mx>
2022-06-09 16:08:37 -0500

    Actualización de pruebas cypress

--- cypress/integration/login/iniciarSesion.spec.js
+++ cypress/integration/login/iniciarSesion.spec.js
@@ -1,8 +1,14 @@
 describe('Carga la plataforma educ', () => {
 
-    it('Usuario: Visita la url del sitio de educ', () => {
+    it('Usuario: Visita la url del sitio de DEV educ', () => {
         
         cy.visit('http://deveduc.ddns.net:88/index.demo')
+
+    })
+
+    it('Usuario: Visita la url del sitio de PRUEBAS educ', () => {
+        
+        cy.visit('http://educ.ddns.net:88/index.demo')
 
     })
 
